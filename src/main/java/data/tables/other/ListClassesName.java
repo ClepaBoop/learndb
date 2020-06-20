ce48815487ec32d6d8227166e654e58557d9c372
ClepaBoop <neondertalez0@gmail.com>
2020-06-20 17:18:02 +0700

    create changedb

--- src/main/java/data/tables/other/ListClassesName.java
+++ src/main/java/data/tables/other/ListClassesName.java
@@ -10,17 +10,27 @@ import java.util.regex.Pattern;
 
 public class ListClassesName {
 
+    public static void main(String[] args) throws IOException {
+        ListClassesName name = new ListClassesName("./src/main/java/data/tables");
+        name.classesName().forEach(s -> System.out.println(s));
 
-    public static ArrayList<String> classesName(String path) throws IOException {
+    }
+
+    private String path;
+    public ListClassesName(String path) {
+        this.path = path;
+    }
+
+    public ArrayList<String> classesName() throws IOException {
 //        "./src/main/java/data/tables"
         ArrayList<String> list = new ArrayList<>();
 
         Path dir = Paths.get(path);
         DirectoryStream<Path> stream = Files.newDirectoryStream(dir);
 
-            Pattern pattern = Pattern.compile(".java", Pattern.CASE_INSENSITIVE);
+            Pattern pattern = Pattern.compile(".java$", Pattern.CASE_INSENSITIVE);
             for (Path file : stream) {
-                String str = file.getFileName().toString();
+                String str = file.toString();
                 Matcher match = pattern.matcher(str);
                 if (match.find()) {
                     list.add(str);
